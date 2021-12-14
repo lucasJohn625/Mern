4ae078ac1d1e244ab8da0595146ea5b67f9f4e5f
lucasJohn625 <millerlucas625@gmail.com>
2021-12-14 12:49:11 -0800

    app.js

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>
+          Hello Dojo!
+        </h1>
+        <h3>
+          Thing I need to do:
+        </h3>
+        <ol>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+        </ol>
+      </header>
+        
+    </div>
+  );
+}
+
+export default App;
