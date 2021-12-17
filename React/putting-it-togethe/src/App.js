fcab5fa75b3df4a642a6c5288ffb12a00dd3eb0e
lucasJohn625 <millerlucas625@gmail.com>
2021-12-16 16:41:19 -0800

    PuttingItTogether

---
+++ React/putting-it-togethe/src/App.js
@@ -0,0 +1,56 @@
+
+import './App.css';
+import Child from './components/Child'
+
+function App() {
+
+  const coolObject = [
+      {
+          firstName: "Jane",
+          lastName: "Doe",
+          Age: 45,
+          hairColor: "Black",
+          
+      },
+      {
+        firstName: "John",
+        lastName: "Smith",
+        Age: 99,
+        hairColor: "Brown",
+        
+      },
+  ];
+  //const [first, second] = coolObject;
+  
+  return (
+  
+
+    <div className="App">
+      {
+      coolObject.map((member, index)=>{
+        return(
+          <Child 
+            key={index}
+            firstName={member.firstName}
+            lastName={member.lastName}
+            Age={member.Age}
+            hairColor={member.hairColor}
+                    
+          />
+        )
+      })
+    }
+    </div>
+    
+        /*decondstructing
+    <div className="App">
+      <Child coolObject={first} />
+      <Child coolObject={second}/>
+
+      */
+      
+   
+  );
+}
+
+export default App;
---
+++ React/putting-it-togethe/src/components/Child.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+
+
+const Child = (props) =>{
+    const { firstName, lastName, Age, hairColor} = props;
+
+    const [stateAge, setStateAge] = useState(Age);
+    
+
+        return(
+            <div>
+                <h2>
+                    {lastName}, {firstName}
+                </h2>
+                
+                <p>Age: {stateAge}</p>
+                <p>Hair Color: {hairColor}</p>
+            
+                <button onClick={() => setStateAge(stateAge + 1)}>
+                    Birthday Button for {firstName} {lastName}
+                </button>
+            </div>
+        );
+}
+
+
+export default Child;
